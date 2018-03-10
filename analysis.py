f06c40a69ee288dc8131c4d03cef6191823e10f2
myluo <lmy0217@126.com>
2018-03-10 14:34:06 +0800

    add analysis.py

---
+++ analysis.py
@@ -0,0 +1,41 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description='FashionAI Analysis')
+parser.add_argument('--model', type=str, default='resnet34', metavar='M', help='model name')
+args = parser.parse_args()
+
+log_file = 'main.log'
+
+with open(log_file, 'r') as f:
+    flogs = f.readlines()
+
+trainset = {
+    'loss' : [],
+}
+
+testset = {
+    'accuracy': [],
+    'loss': [],
+}
+
+for flog in flogs:
+    flog = flog.split()
+    if flog[0] == 'Train' and flog[4] == '(0%)]':
+        trainset['loss'].append(float(flog[6]))
+    elif flog[0] == 'Test':
+        temp = flog[6].split('/')
+        testset['accuracy'].append(float(temp[0]) / float(temp[1]))
+        testset['loss'].append(float(flog[4].split(',')[0]))
+
+epochs = len(trainset['loss'])
+x = np.linspace(1, epochs, epochs, endpoint=True)
+plt.figure("Analysis")
+plt.subplot(311)
+plt.plot(x, np.array(trainset['loss']))
+plt.subplot(312)
+plt.plot(x, np.array(testset['loss']))
+plt.subplot(313)
+plt.plot(x, np.array(testset['accuracy']))
+plt.show()
