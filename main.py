62ecc25256e101853532a34c3b15d5651f2b8056
unknown <lmy0217@126.com>
2018-03-23 12:48:24 +0800

    change net model

--- main.py
+++ main.py
@@ -51,19 +51,35 @@ test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_si
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(180, 50)
-        self.fc2 = nn.Linear(50, FashionAI.AttrKey[args.attribute])
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 10, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(10, 16, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(32 * 6 * 6, 100),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(100, 100),
+            nn.ReLU(inplace=True),
+            nn.Linear(100, FashionAI.AttrKey[args.attribute]),
+        )
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 180)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
+        x = self.features(x)
+        x = x.view(x.size(0), 32 * 6 * 6)
+        x = self.classifier(x)
         return F.log_softmax(x, dim=1)
 
 
